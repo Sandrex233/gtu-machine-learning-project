00722855c5782cb425b0566001a20a46f0fe6d2e
Sandrex233 <sandrex233@gmail.com>
2023-11-25 17:35:54 +0400

    data exploration

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import pandas as pd
 df = pd.read_csv("data/pizzaplace.csv")
 
 print(df.head())
+
+print(df.info())
+
+print(df.describe())
+
+print(df.isna().sum()) # no null values
+
+print(df['size'].value_counts())
+
+print(df['name'].value_counts())
