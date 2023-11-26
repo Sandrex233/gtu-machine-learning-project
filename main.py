ff87822d0e59661b041a1a2e3f24b77902d5eb18
Sandrex233 <sandrex233@gmail.com>
2023-11-26 16:59:15 +0400

    v

--- main.py
+++ main.py
@@ -15,3 +15,5 @@ print(df['name'].value_counts())
 print(df["type"].value_counts())
 
 print(df["time"].str.match(r'\d{2}:\d{2}:\d{2}').all())
+
+print(df["date"].str.match(r'\d{4}-\d{2}-\d{2}').all())
